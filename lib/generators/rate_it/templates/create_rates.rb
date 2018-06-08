2c2838d75f85bcad432b1f68d86a525eca8e79ac
Julia Jochum <jj@denkungsart-gmbh.de>
2018-06-08 12:26:10 +0200

    Add rates table to migration, ref #1

--- lib/generators/rate_it/templates/create_rates.rb
+++ lib/generators/rate_it/templates/create_rates.rb
@@ -1,3 +1,16 @@
 class RateItCreateRates < ActiveRecord::Migration[4.2]
-  def change; end
+  def change
+    create_table :rates do |t|
+      t.belongs_to :rater, polymorphic: true
+      t.belongs_to :rateable, polymorphic: true
+      t.decimal :score, precision: 10, scale: 5, null: false
+      t.string :criterion, default: nil
+      t.integer :weight, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :rates, %i[rater_id rater_type criterion]
+    add_index :rates, %i[rateable_id rateable_type criterion]
+  end
 end
